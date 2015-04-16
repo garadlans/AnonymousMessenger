c5d5aa52ec783ed87bb4fc5be7b921bd428f520a
Ваня <vanyairshoff@gmail.com>
2015-04-16 23:48:27 +0300

    Накидано базовое представление

--- MessengerServer/MessengerCore/MessengerCore/MessengerCore.cs
+++ MessengerServer/MessengerCore/MessengerCore/MessengerCore.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using MessengerCoreLib;
 
 namespace MessengerCore
 {
@@ -10,6 +12,11 @@ namespace MessengerCore
     {
         static void Main(string[] args)
         {
+            using (var host = new ServiceHost(typeof(MessengerCoreLib.CoreService)))
+            {
+                host.Open();
+                Console.WriteLine("Host Start line");
+            }
         }
     }
 }
--- MessengerServer/MessengerCore/MessengerCoreLib/CoreService.cs
+++ MessengerServer/MessengerCore/MessengerCoreLib/CoreService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
@@ -10,8 +11,31 @@ namespace MessengerCoreLib
     // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "CoreService" в коде и файле конфигурации.
     public class CoreService : ICoreService
     {
-        public void DoWork()
+       
+
+        public User Login(string username)
         {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetUsers(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMessage(Message message)
+        {
+            OperationContext.Current.GetCallbackChannel<ICoreServiceCallBack>().SendMessage(new Message(1,1,"sample", new DateTime()));
+        }
+
+        public string GetUserName(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Message> GetMessages(int sender, int reciever)
+        {
+            throw new NotImplementedException();
         }
     }
 }
