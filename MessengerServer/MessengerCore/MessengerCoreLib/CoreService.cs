148fd7048e24a726953f95a018e92665f0f21292
Ваня <vanyairshoff@gmail.com>
2015-04-16 22:32:57 +0300

    Технический коммит

---
+++ MessengerServer/MessengerCore/MessengerCoreLib/CoreService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MessengerCoreLib
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "CoreService" в коде и файле конфигурации.
+    public class CoreService : ICoreService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
