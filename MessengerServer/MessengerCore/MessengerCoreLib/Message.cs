e8f1e546ff9833a17fd000efe7d6c11acd64dd9e
Ваня <vanyairshoff@gmail.com>
2015-04-16 21:55:01 +0300

    Начата работа над сервером.
    Добавлены базовые классы.
    Читается литература по WCF

---
+++ MessengerServer/MessengerCore/MessengerCoreLib/Message.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MessengerCoreLib
+{
+    [Serializable]
+    public class Message : ISerializable
+    {
+        public Message(long sender, long getter, string text, DateTime time)
+        {
+            SenderId = sender;
+            RecipientId = getter;
+            Text = text;
+            Time = Time;
+        }
+
+        public long SenderId { get; set; }
+        public long RecipientId { get; set; }
+        public string Text { get; set; }
+        public DateTime Time { get; set; }
+
+        void ISerializable.GetObjectData(SerializationInfo oInfo, StreamingContext oContext)
+        {
+            oInfo.AddValue("SenderId", SenderId);
+            oInfo.AddValue("RecipientId", RecipientId);
+            oInfo.AddValue("Text", Text);
+            oInfo.AddValue("Time", Time.ToShortDateString() + "-" + Time.ToLongTimeString());
+        }
+    }
+}
