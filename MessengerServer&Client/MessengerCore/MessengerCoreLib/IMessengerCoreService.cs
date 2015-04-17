2bed3f394058bb89c2ee51d3b1b1a76db6753a33
Ваня <vanyairshoff@gmail.com>
2015-04-18 00:57:48 +0300

    Клиент и сервер в одном солюшене.
    Сделан интерфейс для базы данных.
    
    Изуаются технологии свящанные с использованием MySQL.Data

---
+++ MessengerServer&Client/MessengerCore/MessengerCoreLib/IMessengerCoreService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MessengerCoreLib
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "ICoreService" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface ICoreService
+    {
+        [OperationContract]
+        User Login(string username);
+
+        [OperationContract]
+        IEnumerable<User> GetUsers(int userId);
+      
+        [OperationContract]
+        string GetUserName(int userId);
+
+        [OperationContract]
+        IEnumerable<Message> GetMessages(int sender, int reciever);
+
+        [OperationContract]
+        void SendMessage(Message message);
+    }
+}
---
+++ MessengerServer&Client/MessengerCore/MessengerCoreLib/MessengerCoreService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MessengerCoreLib
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "MessengerCoreService" в коде и файле конфигурации.
+    public class MessengerCoreService : IMessengerCoreService
+    {
+       
+
+        public User Login(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetUsers(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMessage(Message message)
+        {
+            OperationContext.Current.GetCallbackChannel<ICoreServiceCallBack>().SendMessage(new Message(1,1,"sample", new DateTime()));
+        }
+
+        public string GetUserName(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Message> GetMessages(int sender, int reciever)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
