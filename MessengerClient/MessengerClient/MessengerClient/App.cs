f75c2d4dfd43565d1c8d02ebedce1ff02b3f9c73
garadlans <garadlans@Gmail.com>
2015-04-22 21:28:22 +0400

    I begin my work after Vanya! Oh Yeah!!!

---
+++ MessengerClient/MessengerClient/MessengerClient/App.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerClient
+{
+    class App
+    {
+        //Oh Yeah, I begin working withv the hometask!!!
+    }
+}
