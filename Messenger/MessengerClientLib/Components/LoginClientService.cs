3fe15489a22282c950566480135a9bb121c51de4
Ваня <vanyairshoff@gmail.com>
2015-05-06 11:24:27 +0300

    Добавлены презентеры
    LoginPresenter

--- Messenger/MessengerClientLib/Components/LoginClientService.cs
+++ Messenger/MessengerClientLib/Components/LoginClientService.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MessengerClientLib.Interfaces;
+using MessengerClientLib.MessengerServiceReference;
 
 namespace MessengerClientLib.Components
 {
-    class LoginClientService
+    public class LoginClientService : ILoginClientService
     {
+        public MessengerServiceReference.IMessengerCoreService Client;
+
+        public LoginClientService()
+        {
+            Client = new MessengerCoreServiceClient();
+        }
+        public User Login(string loginName)
+        {
+           return Client.Login(loginName);
+        }
     }
 }
--- Messenger/MessengerClientLib/Presenter/LoginPresenter.cs
+++ Messenger/MessengerClientLib/Presenter/LoginPresenter.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MessengerClientLib.Common;
+using MessengerClientLib.Components;
+using MessengerClientLib.Events;
+using MessengerClientLib.Interfaces;
+using MessengerClientLib.MessengerServiceReference;
 
 namespace MessengerClientLib.Presenter
 {
-    class LoginPresenter
+    public class LoginPresenter : BasePresenter<ILoginWindow>
     {
+        private readonly ILoginClientService _loginService;
+
+        public LoginPresenter(IApplicationController controller, ILoginWindow view, ILoginClientService loginService) : base(controller, view)
+        {
+            _loginService = new LoginClientService();
+            View.LoginAction += RunLogin;
+      
+        }
+
+        private void RunLogin(object sender, LoginArgs e)
+        {
+            var logged = _loginService.Login(e.Username);
+            if (logged != null)
+            {
+                  //TODO
+//                Controller.Run<MessengerPresenter, User>(logged);
+                View.Close();
+            }
+        }
     }
 }
